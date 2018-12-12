b9ea17ade49fa415d5831c6f4c331fb747b8974b
Miguelo0098 <i62rarum@uco.es>
2018-12-12 13:25:07 +0100

    1st version done

---
+++ practicas/practica4/clientCalc.java
@@ -0,0 +1,40 @@
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+import org.apache.axis.encoding.XMLType;
+
+import javax.xml.rpc.ParameterMode;
+
+public class ClienteCalc
+{
+   public static void main(String [] args) throws Exception {
+
+       String endpoint = "http://servidorprueba.com:8080/axis/Calculadora.jws";
+
+       if (args == null || args.length != 3) {
+           System.err.println("Uso: ClienteCalc <sumar|restar> arg1 arg2");
+           return;
+       }
+
+       String method = args[0];
+       if (!(method.equals("sumar") || method.equals("restar"))) {
+           System.err.println("Uso: ClienteCalc <sumar|restar> arg1 arg2");
+           return;
+       }
+
+       Integer i1 = new Integer(args[1]);
+       Integer i2 = new Integer(args[2]);
+
+       Service  service = new Service();
+       Call     call    = (Call) service.createCall();
+
+       call.setTargetEndpointAddress( new java.net.URL(endpoint) );
+       call.setOperationName( method );
+       call.addParameter( "op1", XMLType.XSD_INT, ParameterMode.IN );
+       call.addParameter( "op2", XMLType.XSD_INT, ParameterMode.IN );
+       call.setReturnType( XMLType.XSD_INT );
+
+       Integer ret = (Integer) call.invoke( new Object [] { i1, i2 });
+
+       System.out.println("Resultado : " + ret);
+   }
+}
