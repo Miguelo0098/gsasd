42da0af5dbc376b4e4303d3d2e7731379e7cec8f
Miguelo0098 <i62rarum@uco.es>
2018-12-13 20:44:13 +0100

    terminada practica4 y se acabo

---
+++ practicas/practica4/Calculadora.java
@@ -0,0 +1,18 @@
+
+public class Calculadora{
+        public int sumar(int x, int y){
+                return x + y;
+        }
+
+        public int restar(int x, int y){
+                return x - y;
+        }
+
+        public int multiplicar(int x, int y){
+                return x * y;
+        }
+
+        public int dividir(int x, int y){
+                return x/y;
+        }
+}
